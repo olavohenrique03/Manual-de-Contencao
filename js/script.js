c318806803659924adbad9ee0488b939f70f682f
olavohenrique03 <olavo060103@gmail.com>
2026-05-24 18:24:15 -0300

    route adjustment (correction)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-const BASE_PATH = "/Proj_Joana/";
+const scriptPath = document.currentScript?.src || '';
+const BASE_PATH = scriptPath.includes('github.io') 
+  ? '/Proj_Joana/' 
+  : '/';
 
-fetch( BASE_PATH + "components/header.html")
+// Expor BASE_PATH globalmente
+window.BASE_PATH = BASE_PATH;
+
+fetch(BASE_PATH + "components/header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
+    
+    // Ajustar todos os links relativos para usar BASE_PATH
+    document.querySelectorAll("#header a").forEach(link => {
+      let href = link.getAttribute("href");
+      if (href.startsWith("./")) {
+        link.setAttribute("href", BASE_PATH + href.substring(2));
+      }
+    });
   });
 
-fetch( BASE_PATH + "./components/footer.html")
+fetch(BASE_PATH + "components/footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
