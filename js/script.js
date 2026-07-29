3b4f37ca5c605f82deb2ebd5cecb43cf996844a6
olavohenrique03 <olavo060103@gmail.com>
2026-07-28 22:01:53 -0300

    adjustment the rotes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
 const scriptPath = document.currentScript?.src || '';
-const BASE_PATH = scriptPath.includes('github.io') 
-  ? '/Manual-de-Contencao/' 
+const BASE_PATH = scriptPath.includes('github.io')
+  ? '/Manual-de-Contencao/'
   : '/';
 
-// Expor BASE_PATH globalmente
 window.BASE_PATH = BASE_PATH;
+
+function fixRelativePaths(containerSelector) {
+  document.querySelectorAll(containerSelector + " a, " + containerSelector + " img").forEach(el => {
+    ["href", "src"].forEach(attr => {
+      const value = el.getAttribute(attr);
+      if (!value) return;
+
+      if (value.startsWith("./")) {
+        el.setAttribute(attr, BASE_PATH + value.slice(2));
+      } else if (value.startsWith("../")) {
+        el.setAttribute(attr, BASE_PATH + value.slice(3));
+      }
+    });
+  });
+}
 
 fetch(BASE_PATH + "components/header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
-    
-    // Ajustar todos os links relativos para usar BASE_PATH
-    document.querySelectorAll("#header a").forEach(link => {
-      let href = link.getAttribute("href");
-      if (href.startsWith("./")) {
-        link.setAttribute("href", BASE_PATH + href.substring(2));
-      }
-    });
+    fixRelativePaths("#header");
   });
 
 fetch(BASE_PATH + "components/footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
+    fixRelativePaths("#footer");
   });
